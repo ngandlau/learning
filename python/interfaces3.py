292620008b5001f53ba4d8c812f0f594b415e0ab
Nils Gandlau <28842354+ngandlau@users.noreply.github.com>
2022-09-05 14:43:12 +0200

    small fixes

--- python/interfaces3.py
+++ python/interfaces3.py
@@ -1,30 +1,19 @@
 """
-Example: Abstract class attributes
+An abstract base class with a required *abstract attribute*
 """
 
-from abc import ABC, abstractproperty, abstractmethod
+from abc import ABC, abstractmethod
+from typing import Optional
 
 class Kpi(ABC):
-    # @abstractproperty
-    # def some_attribute(self):
-    #     pass
-
-    def some_method(self):
-        pass
-
+    @property
     @abstractmethod
-    def another_method(self):
+    def required_attribute(self):
         pass
 
 
 class GoodKpi(Kpi):
-    some_attribute: int = None
-
-    def some_method(self):
-        return "some method"
-
-    def another_method(self):
-        return "another method"
+    required_attribute: Optional[int] = None
 
 
 class BadKpi(Kpi):
@@ -34,5 +23,5 @@ if __name__ == '__main__':
     # this can be instantiated
     good_kpi = GoodKpi()
 
-    # this can't be instantiated, because it hasn't implemented preprocess_data() 
+    # this can't be instantiated, because it's missing the required attribute
     bad_kpi = BadKpi()
